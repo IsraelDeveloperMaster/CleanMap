ac833be0e07ea5ce3cd5158ea2204b3f3e13e3ba
Israel Santos <dev@developermaster.net>
2024-10-31 12:18:30 +0100

    marcadores

---
+++ app/src/main/java/net/developermaster/cleanmap/model/dataClassLatitudeLongitude.kt
@@ -0,0 +1,12 @@
+package net.developermaster.cleanmap.model
+
+class dataClassLatitudeLongitude {
+
+    private var latitude: Double = 0.0
+    private var longitude: Double = 0.0
+/*
+    constructor(latitude: Double, longitude: Double) {
+        this.latitude = latitude
+        this.longitude = longitude
+    }*/
+}
